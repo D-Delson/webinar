80c9b40c997c70e551cc57b8b4a34f795e3d7ffc
D-Delson <delson150203@gmail.com>
2025-12-17 00:30:39 +0530

    fixed url issue

--- utils/getHostUrl.ts
+++ utils/getHostUrl.ts
@@ -1,6 +1,6 @@
 export const getHostAPIUrl = () => {
   if (process.env.NEXT_PUBLIC_NODE_ENV === "production")
-    return "https://turais.in";
+    return "https://turais.in/";
   else if (process.env.NEXT_PUBLIC_NODE_ENV === "development")
     return "http://localhost:8000/";
 
